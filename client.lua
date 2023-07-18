06dd63c29a665e0f43eabe70fafedb31588d453e
hoaaiww <75149340+hoaaiww@users.noreply.github.com>
2023-07-18 19:48:45 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,97 @@
+isDead = false
+smoking = false
+smokingIntro = false
+smokePrecentage = 100.0
+
+AddEventHandler('esx:onPlayerDeath', function(data) 
+    isDead = true 
+
+    if smoking or smokingIntro then
+        SendNUIMessage({ type = 'show', show = false })
+        smoking = false
+        smokingIntro = false
+    end
+end)
+
+AddEventHandler('esx:onPlayerSpawn', function() 
+    isDead = false 
+end)
+
+RegisterNetEvent('hoaaiww_smoking:startSmoking', function()
+    local playerPed = PlayerPedId()
+
+    if IsPedArmed(playerPed, 4) or IsPedArmed(playerPed, 1) or IsPedArmed(playerPed, 2) then
+        SetCurrentPedWeapon(playerPed, `WEAPON_UNARMED`, true)
+        Wait(1500) -- Wait just in case if you are using weapon drawing animations
+    end
+
+    smokingIntro = true
+
+    ClearPedTasksImmediately(playerPed)
+    TaskStartScenarioInPlace(playerPed, 'WORLD_HUMAN_SMOKING', 0, true)
+
+    local waited = 0
+    repeat
+        if smokingIntro then -- checks if the player cancelled the smoking before he would lit the cigarette
+            waited = waited + 250
+
+            Wait(250)
+        else
+            break
+        end
+    until waited == 11000
+
+    if smokingIntro then
+        SendNUIMessage({ type = 'show', show = true })
+        smoking = true
+        smokingIntro = false
+        smokePrecentage = 100.0
+    end
+end)
+
+CreateThread(function()
+    while true do
+        if smoking then
+            if (smokePrecentage > 31.1) then
+                smokePrecentage = smokePrecentage - (Config.smokingSpeed / 25)
+
+                SendNUIMessage({ type = 'setSmokePrecentage', smokePrecentage = smokePrecentage })
+            else
+                stopSmoking()
+            end
+        else
+            Wait(500)
+        end
+
+        Wait(250)
+    end
+end)
+
+CreateThread(function()
+    while true do
+        if smoking or smokingIntro then
+            ESX.ShowHelpNotification(Config.Texts['cancel'])
+
+            if IsControlJustPressed(0, Config.CancelButton) then
+                stopSmoking()
+            end
+        else
+            Wait(500)
+        end
+        Wait(0)
+    end
+end)
+
+function stopSmoking()
+    local playerPed = PlayerPedId()
+    
+    if smokingIntro then
+        ClearPedTasksImmediately(playerPed)
+    else
+        ClearPedTasks(playerPed)
+    end
+
+    smokingIntro = false
+    smoking = false
+    SendNUIMessage({ type = 'show', show = false })
+end
---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {}
+Config.ItemsName = {
+    ['Cigaretta'] = 'cigaretta',
+
+    -- Add more like this:
+    -- ['Item Label'] = 'Item Name'
+    -- for example: ['weed'] = 'Marijuana'
+}
+
+Config.CancelButton = 323 -- X
+
+Config.smokingSpeed = 5 -- The smoking time (1 = very slow, 5 = average, 10 = fast, 10+ = very fast and more)
+
+Config.Texts = {
+    ['used_item']           = 'You have pick out a %s from your pocket.',
+    ['cancel']              = 'Press ~INPUT_REPLAY_TIMELINE_PICKUP_CLIP~ to stop smoking',
+    ['mask_not_allowed']    = 'You can\'t smoke in this mask!'
+}
+
+Config.CheckForUpdates = true
